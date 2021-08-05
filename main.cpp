bfe391e1124970787826a124c360673003e1f541
bepissimo <84045543+bepissimo@users.noreply.github.com>
2021-08-05 17:52:16 -0600

    File Interpretation Updated
    Program can now read the file and interpret it.  Saving data to LL is still bugged.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 
 using namespace std;
 
-void csvSort(double min, double max, string fileName, node &root);
-
 struct node
 {
     int id;
-    int size;
+    double size;
     node* next;
 };
+
+
+void csvSort(double min, double max, string fileName, node* focusNode);
+int split (string splittee, char separator, string array[], int size);
+bool isValidNumber (string testString);
 
 int main()
 {
@@ -22,16 +25,23 @@ int main()
 
     string fileName;
     node* root = new node;
+    node* cursor = root;
 
     cout << "Please enter csv file name: ";
     getline(cin, fileName);
     
     csvSort(acreMin, acreMax, fileName, root);
 
+    cout << "Parcels that fit the designated parameters:" << endl;
+    while (cursor->next != nullptr)
+    {
+        cout << "ID: " << cursor->id << " " << "Acreage: " << cursor->size << endl;
+    }
+
     return 0;
 }
 
-void csvSort(double min, double max, string fileName, node* root)
+void csvSort(double min, double max, string fileName, node* focusNode)
 {
     ifstream file;
     file.open(fileName);
@@ -39,5 +49,76 @@ void csvSort(double min, double max, string fileName, node* root)
     if (file.fail())
     {
         cout << "Invalid file name! " << endl;
+        return;
+    }
+
+    string text = "";
+    while (getline(file, text))
+    {
+        if (!(text[0] == '0' && text[1] == ',') && text[0] != 'p') // Accounts for blank parcels and initial declaration
+        {
+            string stringData[100];
+            split(text, ',', stringData, 100);
+            if (isValidNumber(stringData[5]))
+            {
+                cout << text << endl << endl;
+                cout << "STRING DATA: " << stringData[5] << endl;
+                double acreage = stod(stringData[5]);
+                if (acreage > min && acreage < max)
+                {
+                    focusNode->id = stoi(stringData[0]);
+                    focusNode->size = acreage;
+                    focusNode->next = new node;
+                    focusNode = focusNode->next;
+                }
+            }
+        }
+    }
+}
+
+int split (string splittee, char separator, string array[], int size)
+{
+    int arrayColumn = 0;
+    string builder = "";
+    
+    for (int i = 0; i < splittee.length(); i++)
+    {
+        char seeker = splittee[i];
+        if (seeker == separator)
+        {
+            array[arrayColumn] = builder;
+            builder = "";
+            arrayColumn += 1;
+        }
+        else
+        {
+            builder += seeker;
+        }
+    }
+    array[arrayColumn] = builder;
+
+    if (size < arrayColumn + 1)
+    {
+        return -1;
+    }
+    else if (splittee.length() > 0)
+    {
+        return arrayColumn+1;
+    }
+    else
+    {
+        return arrayColumn;
+    }
+}
+
+bool isValidNumber (string testString)
+{
+    if (testString[0] >= '0' && testString[0] <= '9')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
